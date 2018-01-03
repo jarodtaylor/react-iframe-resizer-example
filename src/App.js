2f3489711ca7e25fbdd283bb8a8b9dc84d380cb7
jarodtaylor <jarodrtaylor@gmail.com>
2018-01-03 10:16:41 -0600

    added iframeresizeroptions for cross origin

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const iframeResizerOptions = { checkOrigin: false };
     return (
       <div className="App2">
         <div className="header"><div className="contain">Header (not part of iframe)</div></div>
         <div className="contain">
           <div className="left-col">
-            <ReactIframeResizer src="//localhost:3000/login.html" />
+            <ReactIframeResizer src="//localhost:3000/login.html" iframeResizerOptions={iframeResizerOptions}/>
           </div>
           <div className="right-col">
             Right Column (not part of iframe)
